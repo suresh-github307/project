3d7b043143d5b7a1f3fd226c7f3c1cdb7299c502
suresh-github307 <54834107+suresh-github307@users.noreply.github.com>
2019-09-03 12:31:43 +0530

    Create takebookserv.java

---
+++ takebookserv.java
@@ -0,0 +1,101 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controller;
+
+import Model.DAO;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.ResultSet;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ *
+ * @author DELL
+ */
+@WebServlet(name = "TakeBookserv", urlPatterns = {"/TakeBookserv"})
+public class TakeBookserv extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        
+        PrintWriter out = response.getWriter();
+        try{
+            HttpSession session=request.getSession();
+        String tbs1=request.getParameter("vb1");
+        String tbs2=request.getParameter("vb2");
+        String tbs3=request.getParameter("vb3");
+        String tbs4=request.getParameter("vb4");
+        String tbs5=(String)session.getAttribute("user");
+        Model.DAO dao=new DAO();
+        int res=dao.tbk(tbs1,tbs2,tbs3,tbs4,tbs5);
+        if(res>0)
+        {
+            response.sendRedirect("user.jsp");
+        }
+        else
+        {
+            response.sendRedirect("SearchBook.jsp");
+        }
+        }catch(Exception e){
+            out.println(e);
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
